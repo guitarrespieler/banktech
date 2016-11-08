ffbe4df7d993702d236914b1ed5824f2422d1259
bence0726 <benze726@gmail.com>
2016-11-08 19:03:51 +0100

    OwnGameCreator implementalas.

--- torpedo/src/main/java/communication/OwnGameCreator.java
+++ torpedo/src/main/java/communication/OwnGameCreator.java
@@ -1,4 +1,6 @@
 package communication;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 /**
  * Új saját játék létrehozásáért felelős osztály.
  * Létrehoz egy új játékot a csapat számára. A létrehozott játék azonosítója a válasz "id" mezőjében található.  
@@ -14,21 +16,25 @@ public class OwnGameCreator {
 	 * Ezen a címen érjük el a szerver oldalt.
 	 */
 	private static String URL_TAG = "game";
+	Communication communication;
 
+	/**
+	 * Saját játékot hoz létre. Az új játék ID-ját elmenti.
+	 */
+	public void createOwnGame(Communication communication) {
+		String response = communication.post("http://server-adress:port/jc16-srv/game");
+		Gson parser = new Gson();
+		JsonObject object = parser.fromJson(response,JsonObject.class);
+		ID = Long.parseLong(object.get("id").toString());
+		throw new UnsupportedOperationException();
+	}
+	
 	public long getID() {
 		return this.ID;
 	}
 
 	public void setID(long ID) {
 		this.ID = ID;
-	}
-
-	/**
-	 * Saját játékot hoz létre. Az új játék ID-ját elmenti.
-	 */
-	public void createOwnGame() {
-		// TODO - implement OwnGameCreator.createOwnGame
-		throw new UnsupportedOperationException();
 	}
 
 }
