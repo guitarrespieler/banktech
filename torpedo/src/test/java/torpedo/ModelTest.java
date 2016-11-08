aa37307874bac0a30910ac8224f12ec47c630127
tibor <tibor@K555-LB>
2016-11-09 00:32:49 +0100

    Tesztosztály létrehozása

---
+++ torpedo/src/test/java/torpedo/ModelTest.java
@@ -0,0 +1,13 @@
+package torpedo;
+/**
+ * Modell tesztelésére létrehozott osztály
+ * @author tibor
+ *
+ */
+public class ModelTest {
+
+	public ModelTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
