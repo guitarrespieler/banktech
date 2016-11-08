865241b57bb44a9d343a5062de8d64b8f140db21
bence0726 <benze726@gmail.com>
2016-11-08 21:29:41 +0100

    communication osztaly implementacio

--- torpedo/src/main/java/communication/Communication.java
+++ torpedo/src/main/java/communication/Communication.java
@@ -6,8 +6,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 
 /**
@@ -24,46 +38,75 @@ public class Communication {
 	 */
 	private static String TEAM_TOKEN = "33A0AE6A32DA0DE431035A9D3521C7AF";
 
+	enum httptype{
+		POST,GET
+	}
 	/**
 	 * A paraméterül kapott TAG-et hozzáfűzi az URL címhez és erre a címre posztol. Visszatérési értékként adja a szerver válaszát.
 	 * @param url_tag Ezt a TAG-et fűzi hozzá az URL címhez.
 	 * @throws IOException 
 	 */
-	public static String post(String urlTag) throws IOException {
-		String url = mainURL+urlTag;
-		URL targetURL = new URL(url);
-		HttpURLConnection connection = null;
-		connection = (HttpURLConnection) targetURL.openConnection();
-		connection.setRequestMethod("POST");
-		connection.setRequestProperty("TEAMTOKEN", TEAM_TOKEN);
-		connection.setUseCaches(false);
-	    connection.setDoOutput(true);
-
-	    //Send request
-	    //try (OutputStream output = connection.getOutputStream()) {
-	    //    output.write(query.getBytes(charset));
-	   // }
-
-	    InputStream is = connection.getInputStream();
-	    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-	    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-	    String line;
-	    while ((line = rd.readLine()) != null) {
-	      response.append(line);
-	      response.append('\r');
-	    }
-	    rd.close();
-	    return response.toString();
+	public static String post(String urlTag) {
+		return apicall(urlTag,httptype.POST);
+	    
 	}
-
 	/**
 	 * Get kérést intéz a szerver felé. A paraméterül kapott URL TAG-et hozzáfűzi az url-hez. Visszatérési értékként megadja a szerver válaszát.
 	 * @param urlTag
 	 */
 	public static String get(String urlTag) {
-		String query = mainURL+urlTag;
-		// TODO - implement Communication.get
-		throw new UnsupportedOperationException();
+		return apicall(urlTag,httptype.GET);
 	}
 
+	private static String apicall(String urlTag, httptype type) {
+		BufferedReader rd = null;
+		StringBuilder strbuilder = null;
+		try {
+			String url = mainURL + urlTag;
+			
+			HttpClient httpclient = HttpClients.createDefault();
+			HttpResponse response = null;
+			if(httptype.POST.equals(type)){
+				response = initpost(url, httpclient);
+			}else{
+				response = initget(url, httpclient);
+			}
+			HttpEntity entity = response.getEntity();
+
+			InputStream is = entity.getContent();
+			rd = new BufferedReader(new InputStreamReader(is));
+			strbuilder = new StringBuilder(); // or StringBuffer if Java version
+												// 5+
+			String line;
+			while ((line = rd.readLine()) != null) {
+				strbuilder.append(line);
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rd.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return strbuilder.toString();
+	}
+	private static HttpResponse initget(String url, HttpClient httpclient) throws IOException, ClientProtocolException {
+		HttpResponse response;
+		HttpGet httpget = new HttpGet(url);
+		httpget.addHeader("TEAMTOKEN", TEAM_TOKEN);
+		response = httpclient.execute(httpget);
+		return response;
+	}
+	private static HttpResponse initpost(String url, HttpClient httpclient)
+			throws IOException, ClientProtocolException {
+		HttpResponse response;
+		HttpPost httppost = new HttpPost(url);
+		httppost.addHeader("TEAMTOKEN", TEAM_TOKEN);
+		response = httpclient.execute(httppost);
+		return response;
+	}
 }
