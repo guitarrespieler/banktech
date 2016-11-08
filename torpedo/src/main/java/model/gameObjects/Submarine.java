cc9ebd220caf8e323651cf51f2a3fa1cb69fa22a
tibor <tibor@K555-LB>
2016-11-08 22:11:15 +0100

    Submarine bővítése attribútumokkal

--- torpedo/src/main/java/model/gameObjects/Submarine.java
+++ torpedo/src/main/java/model/gameObjects/Submarine.java
@@ -1,5 +1,31 @@
 package model.gameObjects;
+
+import com.google.gson.Gson;
+
+import communication.CommException;
+
 public class Submarine {
+	
+	/**
+	 * Itt tároljuk a tengeralattjárónkhoz tartozó adatokat.
+	 */
+	private Entity dataHolder;
+	
+	private Sonar sonar;
+	
+	private Gson gsonRef;
+
+	private long gameID;
+	
+	public Submarine(Entity submarineData,long gameID,Gson gsonObject){
+		dataHolder = submarineData;
+		
+		this.gameID = gameID;
+		
+		gsonRef = gsonObject;
+		
+		sonar = new Sonar(this.gameID, dataHolder.getId(), gsonRef);
+	}
 
 	/**
 	 * A tengeralattjárót mozgathatjuk vele. Paraméterül megadhatjuk neki az elfordulás szögét és a gyorsítás / lassítás mértékét ( tehát nem a kívánt sebességre állítjuk, hanem növeljük vagy csökkentjük azt!)
@@ -24,14 +50,19 @@ public class Submarine {
 		throw new UnsupportedOperationException();
 	}
 
-	public void usePassiveSonar() {
-		// TODO - implement Submarine.usePassiveSonar
-		throw new UnsupportedOperationException();
+	public void usePassiveSonar() throws CommException {
+		sonar.scan();
 	}
 
-	public void activateSonar() {
-		// TODO - implement Submarine.activateSonar
-		throw new UnsupportedOperationException();
+	public void activateSonar() throws CommException {
+		sonar.activate();
+	}
+	
+	/**
+	 * A dataHolder-t a paraméterül kapottra frissíti.
+	 */
+	public void refreshData(Entity submarineData){
+		this.dataHolder = submarineData;
 	}
 
 }
