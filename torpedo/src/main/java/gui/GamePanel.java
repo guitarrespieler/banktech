94dd2f262ff1819b660d975b70bfa49c4a1ddc2c
tibor <tibor@K555-LB>
2016-11-09 11:08:10 +0100

    Gui upgrade

--- torpedo/src/main/java/gui/GamePanel.java
+++ torpedo/src/main/java/gui/GamePanel.java
@@ -3,23 +3,30 @@ package gui;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.LayoutManager;
-
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+
+	private static final long serialVersionUID = 298144871000160168L;
 	
-	public GamePanel(Dimension windowDim) {
-		setSize(windowDim);
+	private static final double xScale = 0.5;
+	private static final double yScale = -0.5;
+	private static final double yTranslate = -800;
+	
+	
+	public GamePanel(Dimension windowDim) {		
+		setPreferredSize(windowDim);
 		
 		repaint();
 	}
 	
 	public void paint(Graphics g){
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.translate(0, 400);
-		g2d.scale(1.4, 1.4);
+		Graphics2D g2d = (Graphics2D) g;		
+		g2d.scale(xScale, yScale);	
+		g2d.translate(0, yTranslate);
 		
+		if(!isPreferredSizeSet())
+			setSize(getPreferredSize());
 		
 		g2d.fillRoundRect(50, 50, 50, 50, 100, 100);
 		
