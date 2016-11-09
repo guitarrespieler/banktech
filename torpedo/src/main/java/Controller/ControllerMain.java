eca6ffde8b820c85114f37425c2b0495efc67161
tibor <tibor@K555-LB>
2016-11-09 17:31:43 +0100

    Controller alapok

---
+++ torpedo/src/main/java/Controller/ControllerMain.java
@@ -0,0 +1,13 @@
+package Controller;
+
+public class ControllerMain {
+
+	public ControllerMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+//TODO Játékhoz csatlakozás
+	}
+
+}
